66b6ea805319fcb6ae9198a91c5097b985962be2
Humberto Alves <hjalves@student.dei.uc.pt>
2014-12-01 10:33:55 +0000

    cleanup

--- prob1.hs
+++ prob1.hs
@@ -2,4 +2,3 @@ myLast :: [a] -> a
 myLast [] = error "Empty list"
 myLast [x] = x
 myLast (x:xs) = myLast xs
-
--- prob2.hs
+++ prob2.hs
@@ -1,5 +1,4 @@
 myButLast :: [a] -> a
-
 myButLast [] = error "Empty list"
 myButLast [x] = error "Only has one element"
 myButLast (x:[_]) = x
